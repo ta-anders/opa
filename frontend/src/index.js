02b6c8a1eae38afc30096efaa5639570fe247833
Thomas Anderson <taanderson95@gmail.com>
2017-12-24 12:58:33 +1000

    Moved example into its own folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Board from './components/Board'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import { observe } from './Game';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const rootEl = document.getElementById('root');
-
-observe(knightPosition =>
-  ReactDOM.render(
-    <Board knightPosition={knightPosition} />,
-    rootEl
-  )
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
