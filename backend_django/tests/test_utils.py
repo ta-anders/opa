d6936b7b74a75bc0c1b8a75ef3dd44f9fe2095cc
Tom Anderson <taanderson95@gmail.com>
2018-03-11 23:20:08 +1000

    More moving stuff around, deleted old backend

--- backend_django/tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from opa.utils import LETTERS, get_random_color
-
-
-def test_get_random_color():
-    ans = get_random_color()
-    assert ans.startswith('#')
-    assert all(l in LETTERS for l in ans[1:])
